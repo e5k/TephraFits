be9b1ab010b6630f13ac814635c3c731968c72e6
Seb work MBP <seb@eskd.net>
2019-04-01 13:45:48 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -47,24 +47,24 @@ CE 			= 20; % 20% error on the distal integration limit of the power law fit
 % Example 5.1: uniform errors on xData and yData and uniform distribution of errors
 thicknessE  = 10; % 10% error on all yData
 areaE 		= 10; % 10% error on all xData
-isopachP 	= tephraFits(area, thickness, {'exponential', 'powerlaw', 'weibull'}, 'BIS', 2, 'C', 200, 'lambdaRange', [.01 100], 'nRange', [.01 100], 'runMode', 'probabilistic', 'nbRuns', 100, 'errorType', 'uniform', 'xError', areaE, 'yError', thicknessE, 'CError',  CE);
+isopachP 	= tephraFits(areaT, thickness, {'exponential', 'powerlaw', 'weibull'}, 'BIS', 2, 'C', 200, 'lambdaRange', [.01 100], 'nRange', [.01 100], 'runMode', 'probabilistic', 'nbRuns', 100, 'errorType', 'uniform', 'xError', areaE, 'yError', thicknessE, 'CError',  CE);
 
 % Example 5.2: specific errors for each value on xData and yData and Normal distribution of errors
 thicknessE  = [10, 10, 20, 20, 30, 30]; % yError varying from 10% to 30%
 areaE 		= [10, 10, 20, 20, 30, 30]; % xError varying from 10% to 30%
-isopachP 	= tephraFits(area, thickness, {'exponential', 'powerlaw', 'weibull'}, 'BIS', 2, 'C', 200, 'lambdaRange', [.01 100], 'nRange', [.01 100], 'runMode', 'probabilistic', 'nbRuns', 100, 'errorType', 'normal', 'xError', areaE, 'yError', thicknessE, 'CError',  CE);
+isopachP 	= tephraFits(areaT, thickness, {'exponential', 'powerlaw', 'weibull'}, 'BIS', 2, 'C', 200, 'lambdaRange', [.01 100], 'nRange', [.01 100], 'runMode', 'probabilistic', 'nbRuns', 100, 'errorType', 'normal', 'xError', areaE, 'yError', thicknessE, 'CError',  CE);
 
 %% Example 6: Eruption classification
 clear;
 % Isopach data
 thickness	= [100, 50, 30, 20, 10, 5]; % Isopach thickness (cm)
 area 		= [7.0, 8.9, 12.3, 17.4, 21.34, 25.4]; % Square-root of area (km)
-isopach  	= tephraFits(area, thickness, {'exponential', 'weibull'}, 'BIS', 3, 'lambdaRange', [.01 100], 'nRange', [.01 100], 'plotType', 'none'); % Isopach fit
+isopach  	= tephraFits(areaT, thickness, {'exponential', 'weibull'}, 'BIS', 3, 'lambdaRange', [.01 100], 'nRange', [.01 100], 'plotType', 'none'); % Isopach fit
 
 % Isopleth data
 diameter	= [0.8, 1.0, 1.6, 2.0, 3.0, 3.2, 4.0]; % Isopleth diameter (cm)
 area 		= [23.7, 22.9, 20.4, 18.3, 14.9, 14.1, 12.2]; % Square-root of area (km)
-isopleth  	= tephraFits(area, diameter, {'exponential', 'weibull'}, 'dataType', 'isopleth', 'lambdaRange', [.1 100], 'nRange', [.1 100], 'plotType', 'none'); % Isopleth fit
+isopleth  	= tephraFits(areaT, diameter, {'exponential', 'weibull'}, 'dataType', 'isopleth', 'lambdaRange', [.1 100], 'nRange', [.1 100], 'plotType', 'none'); % Isopleth fit
 
 % Classification
 tephraFits(isopach, isopleth); % Plot classification
